c0ba86290e195998a5172ed95b1cfaa4d19b401b
heapy <63428380+heapyxyz@users.noreply.github.com>
2024-12-18 20:19:32 +0100

    fix: show proper track duration in messages

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,7 +1,8 @@
 import { IPlaylistInfo, Track } from "moonlink.js"
+import { formatDuration } from "./date.js"
 
 export function trackMessage(title: string, track: Track): string {
-  return `## ${title}\n[**${track.title.replaceAll(/\[|\]/g, "")}**](<${track.url}>)\n-# By **${track.author}** | Duration: **${track.duration}**`
+  return `## ${title}\n[**${track.title.replaceAll(/\[|\]/g, "")}**](<${track.url}>)\n-# By **${track.author}** | Duration: **${formatDuration(track.duration)}**`
 }
 
 export function playlistMessage(
@@ -9,5 +10,5 @@ export function playlistMessage(
   playlist: IPlaylistInfo,
   url: string
 ): string {
-  return `## ${title}\n[**${playlist.name.replaceAll(/\[|\]/g, "")}**](<${url}>)\n-# Duration: **${playlist.duration}**`
+  return `## ${title}\n[**${playlist.name.replaceAll(/\[|\]/g, "")}**](<${url}>)\n-# Duration: **${formatDuration(playlist.duration)}**`
 }
